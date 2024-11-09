cd061fd9194ee6b8da5dad945333c2173bae14bd
alisakovalenko1900 <168113700+alisakovalenko1900@users.noreply.github.com>
2024-11-09 22:39:05 +0200

    connectionString

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require("express"); 
+const {Pool} = require('pg');
 const bodyParser = require("body-parser");
 const app = express();
 
+const pool = new Pool({
+	// user: 'database_07_11_user',
+	// host: 'dpg-csmggadumphs73ahfbs0-a.oregon-postgres.render.com',
+	// database: 'database_07_11',
+	// password: 'foq6GujAa5Kh9OGwgEeb0xX7rJcZaDfO',
+  connectionString: 'postgresql://database_07_11_user:foq6GujAa5Kh9OGwgEeb0xX7rJcZaDfO@dpg-csmggadumphs73ahfbs0-a.oregon-postgres.render.com/database_07_11'
+	// port: 5432
+});
+
 app.use(bodyParser.json());
 
-app.get("/test", (req, res) => {
-  res.json({ message: "Запрос GET выполнен успешно." });
+app.get("/test", async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+		res.json(result.rows);
+  }
+    catch (err){
+      console.log(err)
+    }
 });
 
 app.use((error, req, res, next) => {
