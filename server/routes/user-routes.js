05897b5b162ba6c61f37cd798d3d4d0f2b5cb6a7
alisakovalenko1900 <168113700+alisakovalenko1900@users.noreply.github.com>
2024-11-10 21:29:39 +0200

    создание папки server

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const UsersControllers = require('../controllers/users');
+const UsersControllers = require('../../controllers/users');
 
 router.get('/', UsersControllers.getAllUsers);
 router.get('/:id', UsersControllers.getUserById);
